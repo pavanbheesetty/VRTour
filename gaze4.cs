de79d8e9dc3aef91c442060e95c20093023832ff
pavanbheesetty <33778719+pavanbheesetty@users.noreply.github.com>
2019-01-30 19:03:25 +0530

    Add files via upload

---
+++ gaze4.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+public class gaze4 : MonoBehaviour {
+
+    public float gazeTime = 2f;
+    private float timer =0f;
+    private bool gazedAt;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(gazedAt)
+        {
+            timer += Time.deltaTime;
+            
+            if(timer >= gazeTime)
+            {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0f;
+                SceneManager.LoadScene("5");
+            }
+        }
+    }
+    public void PointerEnter()
+    {
+        //Debug.Log("enter");
+        gazedAt = true;
+        transform.localScale += new Vector3(0.1F, 0.1F, 0.1F);
+
+    }
+
+    public void PointerExit()
+    {
+        timer = 0f;
+        gazedAt = false;
+        transform.localScale -= new Vector3(0.1F, 0.1F, 0.1F);
+    }
+}
+
---
+++ scenemodifier.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenemodifier : MonoBehaviour {
+
+    public void sc()
+    {
+        SceneManager.LoadScene("2");
+    }
+
+    public void sc2()
+    {
+        SceneManager.LoadScene("3");
+    }
+
+    public void sc3()
+    {
+        SceneManager.LoadScene("4");
+    }
+    public void sc4()
+    {
+        SceneManager.LoadScene("5");
+    }
+    public void sc5()
+    {
+        SceneManager.LoadScene("6");
+    }
+    public void sc6()
+    {
+        SceneManager.LoadScene("7");
+
+    }
+    public void sc7()
+    {
+        SceneManager.LoadScene("8");
+    }
+    
+}
